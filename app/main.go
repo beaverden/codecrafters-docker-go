90f63041b0bcefa9071dccc222b452c68b180537
beaverden <rozimovschii.denis@gmail.com>
2023-12-23 16:50:01 +0200

    Isolate

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"path/filepath"
 
 	// Uncomment this block to pass the first stage!
 	"os"
@@ -18,7 +20,26 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	cmd := exec.Command(command, args...)
+	dirpath, _ := os.MkdirTemp("", "test-run")
+	original_path, err := os.Open(command)
+	if err != nil {
+		fmt.Printf("Failed to open original file: %v", err)
+		os.Exit(1)
+	}
+	copied_path, err := os.OpenFile(filepath.Join(dirpath, "executable"), os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		fmt.Printf("Failed to open copy file location: %v", err)
+		os.Exit(1)
+	}
+	io.Copy(copied_path, original_path)
+	original_path.Close()
+	copied_path.Close()
+
+	all_args := []string{dirpath, "./executable"}
+	all_args = append(all_args, args...)
+
+	fmt.Println("Args", all_args)
+	cmd := exec.Command("chroot", all_args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
